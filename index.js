d77ecc56bdd95ff9668648160fb3489639c0f10f
Zunaid Miah <45538749+Zunaid420@users.noreply.github.com>
2022-02-20 18:39:26 +0600

    all files added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    // socket.broadcast.emit('hi');
+    console.log('socket connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg['message']);
+        var channel = msg['channel'];
+        // socket.broadcast.emit('channel', msg);
+        io.emit(channel, msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
